bec2d5eef055583f59dab156b77b43937d6ba6ee
khasan0102 <khasanabdukarimov01@gmail.com>
2021-03-01 13:33:32 +0500

    end videos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 navBtn.onclick = () => {
       navSection.classList.toggle('margin-left-minus');
+      videoList.classList.toggle('videos-list');
+      videoList.classList.toggle('padding-left');
 }
